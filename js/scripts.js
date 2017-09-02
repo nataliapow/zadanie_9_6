79c6d2d3d22d27487ceb095191fcf6cbb52d70d9
Natalia <n.powalska@gmail.com>
2017-09-02 19:31:18 +0200

    Variable modification

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,10 @@
-var add, list, element, listitems, i;
+var add, list, element, listitems;
 add = document.getElementById('addElem');
 list = document.getElementById('list');
+listitems = list.getElementsByTagName('li').length;
 
-add.addEventListener('click', function(e){
+add.addEventListener('click', function(){
 	element = document.createElement('li');
-	element.innerHTML = 'item';	
-	function updateCount(){
-		listitems = list.getElementByTagName('li').length;
-	} 
+	element.innerHTML = 'item' + listitems.length;	
 	list.appendChild(element);
-})
+});
