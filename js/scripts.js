0fa03a7b1b05b84b4a4368baa9a600df9ac974ad
Natalia <n.powalska@gmail.com>
2017-09-02 15:54:05 +0200

    Innitial commit

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var add, list, element, listitems, i;
+add = document.getElementById('addElem');
+list = document.getElementById('list');
+
+add.addEventListener('click', function(e){
+	element = document.createElement('li');
+	element[i].innerHTML = 'item';	
+	function updateCount(){
+		listitems = list.getElementByTagName('li').length;
+	} 
+	list.appendChild(element);
+})
