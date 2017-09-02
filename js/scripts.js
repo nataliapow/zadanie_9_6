ef95979491bf9c307b70a3eff2464db34c463ac3
Natalia <n.powalska@gmail.com>
2017-09-02 19:49:14 +0200

    Remove unnecessary property

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 var add, list, element, listitems;
 add = document.getElementById('addElem');
 list = document.getElementById('list');
-listitems = list.getElementsByTagName('li').length;
+listitems = list.getElementsByTagName('li');
 
 add.addEventListener('click', function(){
 	element = document.createElement('li');
-	element.innerHTML = 'item' + listitems.length;	
+	element.innerHTML = 'item ' + listitems.length;	
 	list.appendChild(element);
 });
